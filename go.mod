18e84fb4adfe06440504cb89c48f0234e7ffdd29
Jung Woo James Jeong <uhdang@gmail.com>
2022-01-11 21:43:27 +0100

    chapter 10 - building an authentication system

--- go.mod
+++ go.mod
@@ -9,4 +9,5 @@ require (
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/lib/pq v1.10.4 // indirect
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
 )
--- models/users.go
+++ models/users.go
@@ -3,6 +3,8 @@ package models
 import (
 	"errors"
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var (
@@ -11,17 +13,53 @@ var (
 
 	// ErrInvalidID is returned when an invalid ID is provided to a method like Delete.
 	ErrInvalidID = errors.New("models: ID provided was invalid")
+
+	// ErrInvalidPassword is returned when an invalid password
+	//is used when attempting to authenticate a user.
+	ErrInvalidPassword = errors.New("models: incorrect password provided")
+	userPwPepper       = "random-string-as-peppr"
 )
 
 type User struct {
 	gorm.Model
-	Name  string
-	Email string `gorm:"not null;unique_index"`
-	Age   int
+	Name         string
+	Email        string `gorm:"not null;unique_index"`
+	Password     string `gorm:"-"`
+	PasswordHash string `gorm:"not null"`
+	Age          int
 }
 
 type UserService struct {
 	db *gorm.DB
+}
+
+// Authenticate can be used to authenticate a user with the
+//provided email address and password.
+// If the email address provided is invalid, this will return
+//		nil, ErrNotFound
+// If the password provided is invalid, this will return
+//		nil, ErrInvalidPassword
+// If the email and password are both valid, this will return
+//		user, nil
+// Otherwise if another error is encountered this will return
+//		nil, error
+func (us *UserService) Authenticate(email, password string) (*User, error) {
+	foundUser, err := us.ByEmail(email)
+	if err != nil {
+		return nil, err
+	}
+
+	err = bcrypt.CompareHashAndPassword(
+		[]byte(foundUser.PasswordHash),
+		[]byte(password+userPwPepper))
+	switch err {
+	case nil:
+		return foundUser, nil
+	case bcrypt.ErrMismatchedHashAndPassword:
+		return nil, ErrInvalidPassword
+	default:
+		return nil, err
+	}
 }
 
 func NewUserService(connectionInfo string) (*UserService, error) {
@@ -74,7 +112,6 @@ func (us *UserService) ByAge(age int) (*User, error) {
 		return nil, err
 	}
 	return &user, err
-
 }
 
 // DestructiveReset drops the user table and rebuilds it
@@ -88,6 +125,14 @@ func (us *UserService) DestructiveReset() error {
 
 // Create will create the provided user and backfill data like the ID, CreatedAt, and UpdatedAt fields.
 func (us *UserService) Create(user *User) error {
+	pwBytes := []byte(user.Password + userPwPepper)
+	hashedBytes, err := bcrypt.GenerateFromPassword(
+		pwBytes, bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user.PasswordHash = string(hashedBytes)
+	user.Password = ""
 	return us.db.Create(user).Error
 }
 
